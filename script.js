ee9db0952ae9b694bda49de98c687c3cd4d221b5
Francois L <francoisly@ymail.com>
2021-07-12 14:00:22 +0200

    lundi 14h

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 const grid = document.getElementById("grid");
-const holes = document.querySelectorAll(".hole");
+// const holes = document.querySelectorAll(".hole");
+const moles = document.querySelectorAll(".moleContainer");
+
 let delay = 1000;
 const coeff = 1.1;
 let counter = 0;
+let score = 0;
+let gameOn = true;
 
-holes.forEach((item) => item.addEventListener('click', doSomething));
+moles.forEach((item) => item.addEventListener('click', killMole));
 
-function doSomething(event) {
-    let hole = event.currentTarget;
-    hole.classList.remove("mole");
+function killMole(event) {
+    let mole = event.currentTarget;
+    if (gameOn ===true) {
+        increaseScore(mole);
+        mole.classList.remove("active");
+        mole.classList.add("getIn");
+        mole.classList.remove("getOut");
+    }
+    
     counter++;
 }
 
+function increaseScore (mole) {
+    if(mole.classList.contains("active")) {
+        score = score + 10;
+    }
+    console.log(score);
+    return score;
+}
+
 function randomHoleSelector() {
-    const holesLeft = document.querySelectorAll(".hole:not(.mole)");
-    if (holesLeft.length === 0) {
+    const molesLeft = document.querySelectorAll(".moleContainer:not(.active)");
+    if (molesLeft.length === 0) {
         return undefined;
     } else {
-        return holesLeft[Math.floor(Math.random() * holesLeft.length)];
+        return molesLeft[Math.floor(Math.random() * molesLeft.length)];
     }
 }
 
 function displayMole(delay) {
 
     let intervalId = setInterval(() => {
+        let randomMole = randomHoleSelector();
         if(checkGameOver()) {
             clearInterval(intervalId);
         };
-        if (randomHoleSelector() === undefined) {
+        if (randomMole === undefined) {
 
         } else {
-            randomHoleSelector().classList.add("mole");
+            // randomMole.children.classList.add("active")
+            randomMole.classList.add("active");
+            randomMole.classList.add("getOut");
+            randomMole.classList.remove("getIn");
+            
         }
 
         if (counter > 10) {
@@ -44,9 +67,11 @@ function displayMole(delay) {
 }
 
 function checkGameOver() {
-    const holesLeft = document.querySelectorAll(".hole:not(.mole)");
+    const holesLeft = document.querySelectorAll(".moleContainer:not(.active)");
     if (holesLeft.length === 0) {
         console.log("perdu");
+         gameOn = false;
+
         return true;
     }
 }
